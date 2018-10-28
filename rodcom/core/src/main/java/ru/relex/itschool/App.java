eafd6cd7bc2e6161bee201ff226f194a5465c11f
tosha <toxatr@icloud.com>
2018-10-28 18:30:09 +0300

    added RC_group and RC_role examples (need to review)

--- rodcom/core/src/main/java/ru/relex/itschool/App.java
+++ rodcom/core/src/main/java/ru/relex/itschool/App.java
@@ -6,6 +6,8 @@ import javax.persistence.Persistence;
 
 import java.util.Date;
 
+import ru.relex.itschool.core.model.Rc_group;
+import ru.relex.itschool.core.model.Rc_role;
 import ru.relex.itschool.core.model.Rc_school;
 import ru.relex.itschool.core.model.Rc_member;
 
@@ -18,29 +20,33 @@ public class App
 	static EntityManagerFactory sessionFactory; 
 
 
-	protected static void setUp() throws Exception {
+	protected static void setUp() {
 		sessionFactory = Persistence.createEntityManagerFactory("RODCOM");
 	}
 
 
     public static void main( String[] args )
     {
-        try {
-			setUp();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-        
+        setUp();
+
         EntityManager em = sessionFactory.createEntityManager();
         em.getTransaction().begin();
 
-        Rc_school school = new Rc_school("МБОУ СОШ №123");
-        em.persist(school);
-        
-        Rc_member member = new Rc_member("Председатель родительского комитета", new Date(), "Иван", "Иванович", "Иванов", "+7-903-111-11-11", "222-22-22", 
-        		                         "г. Воронеж, ул. Бахметьева, д. 2б", "ivanov@relex.ru", "ivanovii", "vk_ivanov", "ok_ivanov", "twit_ivanov");
-        em.persist(member);
+        Rc_group group = new Rc_group();
+        group.setGroupId(2);
+        group.setSchoolId(1);
+        group.setGroupType("group type");
+        group.setGroupName("group name");
+        group.setGroupDesc("group description");
+        group.setEnabled(true);
+
+        em.persist(group);
+
+        Rc_role role = new Rc_role();
+        role.setRoleId(1);
+        role.setRoleName("role");
+
+        em.persist(role);
 
         // Perform finds, execute queries,
         // update entities, etc.
