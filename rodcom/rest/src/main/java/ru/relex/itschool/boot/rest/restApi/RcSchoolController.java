8b4da7c1dedd81392da3c1a3a1512603b884f6fe
Коваль Дмитрий Борисович <koval@relex.ru>
2018-11-08 18:43:45 +0300

    added controller for RcSchool

---
+++ rodcom/rest/src/main/java/ru/relex/itschool/boot/rest/restApi/RcSchoolController.java
@@ -0,0 +1,40 @@
+package ru.relex.itschool.boot.rest.restApi;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.relex.itschool.services.modelDto.RcSchoolDto;
+import ru.relex.itschool.services.service.IRcSchoolService;
+
+@RestController
+@RequestMapping("/school")
+public class RcSchoolController {
+
+    private final IRcSchoolService service;
+
+    public RcSchoolController(IRcSchoolService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/get")
+    RcSchoolDto getById(int id){
+        RcSchoolDto schoolDto = service.getById(id);
+        if (schoolDto == null)
+            throw  new IllegalArgumentException();
+        return schoolDto;
+    }
+
+    @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    RcSchoolDto createSchool(@RequestBody RcSchoolDto schoolDto) {
+        return service.createSchool(schoolDto);
+    }
+
+    @PutMapping("/update")
+    boolean updateSchool(@RequestBody RcSchoolDto schoolDto){
+        return service.updateSchool(schoolDto);
+    }
+
+    @DeleteMapping("/delete")
+    boolean deleteSchool(@RequestBody RcSchoolDto schoolDto){
+        return service.deleteSchool(schoolDto);
+    }
+}
---
+++ rodcom/services/src/main/java/ru/relex/itschool/services/service/impl/RcSchoolServiceImpl.java
@@ -0,0 +1,62 @@
+package ru.relex.itschool.services.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.relex.itschool.db.entity.RcSchool;
+import ru.relex.itschool.db.repository.IRcSchoolRepository;
+import ru.relex.itschool.services.modelDto.RcSchoolDto;
+import ru.relex.itschool.services.service.IRcSchoolService;
+
+import java.util.Optional;
+
+@Service
+public class RcSchoolServiceImpl implements IRcSchoolService {
+
+    private final IRcSchoolRepository repository;
+
+    public RcSchoolServiceImpl(IRcSchoolRepository repository) {
+        this.repository = repository;
+    }
+
+    public RcSchool getSchoolById(int id) {
+        Optional<RcSchool> schoolOptional = repository.findById(id);
+        if (!schoolOptional.isPresent())
+            return null;
+        return schoolOptional.get();
+    }
+
+    @Override
+    public RcSchoolDto getById(int id) {
+        RcSchool s = getSchoolById(id);
+        if (s == null)
+            return null;
+        return new RcSchoolDto(s.getSchool_id(), s.getSchool_name());
+    }
+
+    @Override
+    public RcSchoolDto createSchool(RcSchoolDto schoolDto) {
+        RcSchool school = new RcSchool();
+        school.setSchool_name(schoolDto.getSchool_name());
+        school = repository.save(school);
+        schoolDto.setSchool_id(school.getSchool_id());
+        return schoolDto;
+    }
+
+    @Override
+    public boolean updateSchool(RcSchoolDto schoolDto) {
+        RcSchool school = getSchoolById(schoolDto.getSchool_id());
+        if (school == null)
+            return false;
+        school.setSchool_name(schoolDto.getSchool_name());
+        school = repository.save(school);
+        return true;
+    }
+
+    @Override
+    public boolean deleteSchool(RcSchoolDto schoolDto) {
+        RcSchool school = getSchoolById(schoolDto.getSchool_id());
+        if (school == null)
+            return false;
+        repository.delete(school);
+        return true;
+    }
+}
